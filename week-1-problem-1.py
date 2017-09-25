8579a35ff4164748e816c809c3e842bf447120a8
Berke Emrecan Arslan <berke@beremaran.com>
2017-09-25 16:29:33 +0300

    Center point of disk reacts to image size now

--- week-1-problem-1.py
+++ week-1-problem-1.py
@@ -14,13 +14,13 @@ img = np.zeros((height, width), dtype=np.uint8)
 # Create [x, y] coordinate combinations on grid
 xx, yy = np.mgrid[:height, :width]
 # Mathematical definition of a circle centered at (200,300)
-circle = (xx - 200) ** 2 + (yy - 300) ** 2
+circle = (xx - (height / 2)) ** 2 + (yy - (width / 2))) ** 2
 
 # Set the intensity values
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
-        intensity = circle[x][y]
-        img[x][y] = intensity
+        intensity=circle[x][y]
+        img[x][y]=intensity
 
 # Display Image directly from buffer
 Image.fromarray(img, 'L').show()
