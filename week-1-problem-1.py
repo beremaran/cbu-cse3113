2f872c21f4983e269b4764c4ef10a490d2928ee2
Berke Emrecan Arslan <berke@beremaran.com>
2017-09-25 16:27:28 +0300

    Cleaned and improved comments

--- week-1-problem-1.py
+++ week-1-problem-1.py
@@ -7,27 +7,23 @@ from PIL import Image
 # Image size
 width = 600
 height = 400
-# optional
-#channels = 3
 
 # Create an empty image
 img = np.zeros((height, width), dtype=np.uint8)
-#optionally we can define the array with channel information
-#img = np.zeros((height, width, channels), dtype=np.uint8)
 
-#Create [x, y] coordinate combinations on grid
+# Create [x, y] coordinate combinations on grid
 xx, yy = np.mgrid[:height, :width]
-#Mathematical definition of a circle centered at (200,200)
+# Mathematical definition of a circle centered at (200,300)
 circle = (xx - 200) ** 2 + (yy - 300) ** 2
 
-# Set the RGB values
+# Set the intensity values
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
         intensity = circle[x][y]
         img[x][y] = intensity
 
-#Display Image directly from buffer
+# Display Image directly from buffer
 Image.fromarray(img, 'L').show()
 
-#Save ımage directly from buffer
+# Save ımage directly from buffer
 Image.fromarray(img, 'L').save('circle-bad.png', 'PNG')
